3a4f13ed981ce2499cdad781bae74bcf6fc93dc7
jingkang97 <jingkang@lomotif.com>
2022-07-11 17:21:22 +0800

    main page

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,17 @@
+import {styles} from "./Main.styles"
+const Main = () => {
+    return ( 
+        <div style={styles.Banner}>
+            <header>
+                <h1>
+                    Cookit
+                </h1>
+            </header>
+            <main>
+                <h2>Mingzhen's cookbook</h2>
+            </main>
+        </div>
+     );
+}
+ 
+export default Main;
---
+++ src/pages/Main/Main.styles.js
@@ -0,0 +1,13 @@
+const Banner = {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    fontWeight: "bold",
+    fontSize: "100px",
+}
+
+export const styles = {
+    Banner: Banner,
+}
